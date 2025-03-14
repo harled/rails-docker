ca22fe7acb06f73597b40379a5d18b695f47334d
Chris Young <chris@harled.ca>
2025-03-13 21:54:01 -0400

    feat: support for 3.4.2 and 3.3.7

--- dx.sh
+++ dx.sh
@@ -16,14 +16,14 @@ function help () {
   echo "  images                    - list the available images"
   echo
   echo "example (test and release): "
-  echo "1. install latest packages for rails-dev 3.3.6 image"
-  echo "   > freshen rails-dev 3.3.6"
-  echo "2. after testing, promote latest image to new 3.3.6 tag"
-  echo "   > release rails-dev 3.3.6"
+  echo "1. install latest packages for rails-dev 3.4.2 image"
+  echo "   > freshen rails-dev 3.4.2"
+  echo "2. after testing, promote latest image to new 3.4.2 tag"
+  echo "   > release rails-dev 3.4.2"
   echo
   echo "example (full send, skips testing): "
-  echo "1. build, tag and deploy an updated image rails-dev 3.3.6 image"
-  echo "   > update rails-dev 3.3.6"
+  echo "1. build, tag and deploy an updated image rails-dev 3.4.2 image"
+  echo "   > update rails-dev 3.4.2"
   echo
 }
 
