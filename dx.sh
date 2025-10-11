20eae65420673b1ea196bc4165af924cd0851c79
Chris Young <chris@harled.ca>
2025-10-10 21:34:25 -0400

    chore: ruby 3.4.7

--- dx.sh
+++ dx.sh
@@ -16,14 +16,14 @@ function help () {
   echo "  images                    - list the available images"
   echo
   echo "example (test and release): "
-  echo "1. install latest packages for rails-dev 3.4.6 image"
-  echo "   > freshen rails-dev 3.4.6"
-  echo "2. after testing, promote latest image to new 3.4.6 tag"
-  echo "   > release rails-dev 3.4.6"
+  echo "1. install latest packages for rails-dev 3.4.7 image"
+  echo "   > freshen rails-dev 3.4.7"
+  echo "2. after testing, promote latest image to new 3.4.7 tag"
+  echo "   > release rails-dev 3.4.7"
   echo
   echo "example (full send, skips testing): "
-  echo "1. build, tag and deploy an updated image rails-dev 3.4.6 image"
-  echo "   > update rails-dev 3.4.6"
+  echo "1. build, tag and deploy an updated image rails-dev 3.4.7 image"
+  echo "   > update rails-dev 3.4.7"
   echo
 }
 
--- rails-dev/README.md
+++ rails-dev/README.md
@@ -4,6 +4,7 @@ This image is to be used as the base image for local Ruby on Rails development u
 
 ## Changelog
 
+* 3.4.7 - bump ruby
 * 3.4.6 - bump ruby, bump nodejs version
 * 3.4.5 - bump nodejs version
 * 3.4.4 - removed postgres client (support sqlite3 only)
